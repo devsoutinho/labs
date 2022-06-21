c67f680a39dfca7539aedb7733878b1f56028e3e
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-20 23:41:39 -0300

    update

--- _apps/dscli/commands/app/name.cljs
+++ _apps/dscli/commands/app/name.cljs
@@ -1,17 +1,43 @@
 (ns commands.app.name
-  (:require [clojure.string :as str]
-            [utils.display :refer [display]]
-            [utils.exec :refer [exec]]))
+  (:require [utils.display :refer [display]] 
+            [utils.platform :refer [run-in]] 
+            [clojure.string :as str]
+            ["fs" :as fs]))
 
-(def ^:private command "app name")
+(def config {:manifest-path "app.manifest.json"
+             :name-paths {:android {:path "android/app/src/main/AndroidManifest.xml"
+                                    :pattern "android:label=\"(.+?)\""
+                                    :result (fn [new-name] (str "android:label=\"" new-name "\""))}
+                          #_#_ :ios {:path "ios/Runner/Info.plist" 
+                                :pattern "<key>CFBundleDisplayName</key>\n<string>%%NEW_NAME%%</string>"}}
+             :app-id-paths {:android {:path "android/app/build.gradle"
+                                      :pattern "applicationId \"com.mariosouto.labs\""}}})
+
+; ==================================================
+
+(defn ^:private app-platform-rename [app-platform]
+  (let [new-name "NOVO NOMEEE"
+        app-platform-name (first app-platform)
+        path (str js/process.env.DSCLI_RUNNING_AT "/" (:path (second app-platform)))
+        pattern (:pattern (second app-platform))
+        result (:result (second app-platform))
+        file-content-start (fs/readFileSync path #js {:encoding "utf-8"})
+        file-content-updated (str/replace file-content-start (re-pattern pattern) (result new-name))] 
+    (display (str "[" app-platform-name "] Renaming"))
+    (fs/writeFileSync path file-content-updated #js {:encoding "utf-8"})
+    (display (str "[" app-platform-name "] Done!"))))
+
+
+; ==================================================
+
+(def ^:private command "app set-name")
+
 (defn ^:private action []
-  (let [project_type (cond
-                       (str/includes? (exec "ls -la") "pages") "Next.js"
-                       (str/includes? (exec "ls -la") "pubspec.yaml") "Flutter!"
-                       :else "You must be in a `Next.js` or `Flutter project`")]
-    (display project_type)))
+  (display "Renaming app ...") 
+  (mapv app-platform-rename (get config :name-paths))
+  (display "Rename done!!!"))
 
 (defn define [program]
   (-> program
       (.command command)
-      (.action action)))
+      (.action (run-in [:flutter] action))))
