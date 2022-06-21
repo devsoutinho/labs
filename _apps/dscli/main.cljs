55e882c175b55dac0c2191ed08d882f1d2ad199c
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-21 12:51:20 -0300

    update

--- _apps/dscli/main.cljs
+++ _apps/dscli/main.cljs
@@ -1,9 +1,12 @@
 (ns dscli.main
-  (:require ["./args.js" :refer [args]]
-            ["commander" :refer [Command]]
+  (:require ["commander" :refer [Command]]
             [commands.app.main :as app]
             [utils.display :refer [display]]))
   
+(def args (let [base-args (into [] (filter (fn [arg] (not= arg "./main.cljs")) js/process.argv))]
+            (clj->js (if (= (count base-args) 2)
+                       (conj base-args "-h")
+                       base-args))))
 
 (display "[DsCLI - Start]\n" "bgMagenta")
 
