5df7ec7c949a315d8b3c6415540fec8a86d9c472
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-25 17:54:18 -0300

    update

--- _servers/webproxy/src/webproxy/modules/auth/external_file.cljs
+++ _servers/webproxy/src/webproxy/modules/auth/external_file.cljs
@@ -1,4 +1,7 @@
-(ns src.webproxy.modules.auth.external-file)
+(ns src.webproxy.modules.auth.external-file
+  (:require ["@supabase/supabase-js" :refer [createClient]]))
+
+(prn createClient)
 
 (defn sum []
   (+ 1 1))
