0fca81b5f0db0f6471ccea41f81f203f35a818f8
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-26 13:47:46 -0300

    feat: add login logic

---
+++ _servers/webproxy/src/webproxy/repositories/user.cljs
@@ -0,0 +1,14 @@
+(ns src.webproxy.repositories.user
+  (:require ["@supabase/supabase-js" :refer [createClient]]
+            [promesa.core :as p]))
+
+
+(defn get-all-users []
+  (p/let [supabase-api-key js/globalThis.process.env.SUPABASE_API_KEY
+          supabase-url js/globalThis.process.env.SUPABASE_URL
+          supabase (createClient supabase-url supabase-api-key)
+          response (-> (.from supabase "user")
+                       (.select))
+          response-parsed (js->clj response {:keywordize-keys true})
+          users (:data response-parsed)]
+    users))
