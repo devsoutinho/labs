b203a12bf0b459992f899dbb2c38a44abab4d3b4
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-06 14:24:41 -0300

    update

--- _apps/mariosouto.com/pages/_document.tsx
+++ _apps/mariosouto.com/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="theme-color" content="#FF0000" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
